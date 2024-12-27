5b99a91496dc7ee43a9f566fbe29089bbfa3320b
Jibin Gigi <144718331+Jibin-Gigi@users.noreply.github.com>
2024-12-28 02:44:04 +0530

    Create concat.js

---
+++ concat.js
@@ -0,0 +1,17 @@
+let a="123";
+console.log(a)
+console.log(a.length)
+
+let b="456";
+
+let c=a+b;
+console.log(c)
+console.log(c.length)
+
+let d=a.concat(b + "789");
+console.log(d)
+console.log(d.length)
+
+let e=a.concat(b, "789");
+console.log(e)
+console.log(e.length)
