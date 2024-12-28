9c33302c587d6576d420750a5350c3e397bd2328
Jibin Gigi <144718331+Jibin-Gigi@users.noreply.github.com>
2024-12-29 02:19:35 +0530

    Update main.js

--- Adding HTML list/main.js
+++ Adding HTML list/main.js
@@ -2,10 +2,16 @@ let languages = ["Kotlin", "Javascript", "Ruby"];
 
 let result = [];
 
+// Function to format each language as a list item and add it to the result array
 let addListItems = (value) => result.push("<li>" + value + "</li>");
 
 languages.forEach(addListItems);
 
+// Get the HTML element with the ID 'languages' (assumes this element exists in the DOM)
 let ulElement = document.getElementById("languages");
 
+
+// Update the innerHTML of the 'languages' element by appending the formatted list items
+// The result array is joined into a single string with newlines (\n) for better readability in the HTML source
 ulElement.innerHTML = ulElement.innerHTML + result.join("\n");
+//innerHTML is contents of HTML element
