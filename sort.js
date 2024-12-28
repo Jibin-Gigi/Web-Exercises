010d71d97cd655422e90c5a8fc8d3da90fdf6f49
Jibin Gigi <144718331+Jibin-Gigi@users.noreply.github.com>
2024-12-28 15:39:23 +0530

    Create sort.js

---
+++ sort.js
@@ -0,0 +1,10 @@
+//Sorting 
+
+let countries = ["India", "USA", "Canada", "Australia", "Japan"];
+console.log("Before sorting : " + countries)
+countries.sort()
+console.log("After sorting : " + countries)
+
+//Sorting in descending order
+countries.reverse()
+console.log("After sorting in descending order : " + countries)
