9bfee0b8ee872b71bce13c3b005dc2a54732299f
Jibin Gigi <144718331+Jibin-Gigi@users.noreply.github.com>
2024-12-28 16:58:14 +0530

    Create numbers.js

---
+++ numbers.js
@@ -0,0 +1,9 @@
+// Numbers in Javascript
+
+let a = "8" // 8 is a string
+num = Number(a) // Converting into a number
+console.log(a); //string
+console.log(num); // number
+
+num2 = Number("Hello")
+console.log(num2); //NaN - Not a number
