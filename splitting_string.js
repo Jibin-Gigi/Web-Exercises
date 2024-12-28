c4b9f0b5f5dc68301dbed6ef5f92d9c4532f2f8f
Jibin Gigi <144718331+Jibin-Gigi@users.noreply.github.com>
2024-12-28 15:53:52 +0530

    Create splitting_string.js

---
+++ splitting_string.js
@@ -0,0 +1,11 @@
+//Splitting a string
+
+let essay = "I am learning JavaScript";
+console.log("No of characters in essay: " + essay.length);
+
+let words = essay.split(" ")
+console.log("Words: " + words)
+console.log("No of words: " + words.length)
+
+console.log("Essay : " + essay )
+console.log("No of characters in essay after splitting: " + essay.length);
