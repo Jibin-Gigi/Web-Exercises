1d883dba1f44c824d82499c9524d5192634692fb
Jibin Gigi <144718331+Jibin-Gigi@users.noreply.github.com>
2024-12-28 14:09:33 +0530

    Create includes.js

---
+++ includes.js
@@ -0,0 +1,11 @@
+/* 
+only gmail is allowed
+*/
+
+
+let email = "test@gmail.com";
+if (email.includes("@gmail.com")){
+    console.log("Valid email");
+} else {
+    console.log("Invalid email");
+}
