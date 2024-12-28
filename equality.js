3dc8faaebfd9a762ef9d1185cfc661e15fce478b
Jibin Gigi <144718331+Jibin-Gigi@users.noreply.github.com>
2024-12-28 15:13:41 +0530

    Create equality.js

---
+++ equality.js
@@ -0,0 +1,15 @@
+/* 
+same password is only allowed
+*/
+
+
+let password1 = "123456";
+let password2 = "12345";
+
+if (password1 === password2){
+    console.log("Password is same");
+} else {
+    console.log(password1);
+    console.log(password2);
+    console.log("Password is not same");
+}
