e71677aedfb3e8f2da0d88796e0e22b437c4620d
Jibin Gigi <144718331+Jibin-Gigi@users.noreply.github.com>
2025-01-01 21:53:07 +0530

    Create objectUsingConstructor.js

---
+++ objectUsingConstructor.js
@@ -0,0 +1,13 @@
+function User(name, age, dept) {
+    this.name = name
+    this.age = age
+    this.dept = dept
+}
+
+let u1 = new User("Manu", 23, "CS")
+let u2 = new User("Felix", 21, "CS")
+let u3 = new User("Nagu", 22, "IT")
+
+console.log(u1)
+console.log(u2)
+console.log(u3)
