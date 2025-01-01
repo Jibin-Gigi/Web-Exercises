281cebdd4a48291b246b90f515ad743c7f0ea816
Jibin Gigi <144718331+Jibin-Gigi@users.noreply.github.com>
2025-01-01 22:07:54 +0530

    Create addDeleteUpdateObjects.js

---
+++ addDeleteUpdateObjects.js
@@ -0,0 +1,21 @@
+let u1 = {
+    name : "Felix",
+    age : 22,
+    dept : "CS",
+}
+console.log(u1)
+
+u1.gender = "Male"
+console.log(u1)
+
+u1['income'] = 66999.55
+console.log(u1)
+
+u1["status"] = "single"
+console.log(u1)
+
+u1.dept = "Psychology"
+console.log(u1)
+
+delete u1.age
+console.log(u1)
